3a6eb0c05db9e1d098a877c03ac4cfef82f453bf
Peter Newcomb <662061+petenewcomb@users.noreply.github.com>
2025-05-22 06:03:43 -0400

    allow nil arg to timerp.Put

--- internal/timerp/timerp.go
+++ internal/timerp/timerp.go
@@ -8,11 +8,6 @@ import (
 	"time"
 )
 
-// This implementation relies on [Go 1.23+ behavior] and is therefore not much
-// more than a type-safe wrapper over [sync.Pool].
-//
-// [Go 1.23+ behavior]: https://pkg.go.dev/time#NewTimer
-
 var pool = sync.Pool{
 	New: func() any {
 		return time.NewTimer(0)
@@ -23,6 +18,18 @@ func Get() *time.Timer {
 	return pool.Get().(*time.Timer)
 }
 
+// Safe to call Put with a nil timer, it will be ignored. This simplifies
+// conditional use cases with timer variables, as it's always safe to defer
+// timerp.Put(t) even if the variable was never initialized.
 func Put(t *time.Timer) {
-	pool.Put(t)
+	if t != nil {
+		if !t.Stop() {
+			// Given Go 1.23+, this is just to support asynctimerchan != 0
+			select {
+			case <-t.C:
+			default:
+			}
+		}
+		pool.Put(t)
+	}
 }
